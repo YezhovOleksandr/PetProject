f9275f7db544b64fbccb19fc26992abb76835722
YezhovOleksandr <bladeagainst@gmail.com>
2023-02-08 00:45:49 +0200

    Initial commit

---
+++ src/main/java/com/example/petprojectbackend/PetProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.petprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petprojectbackend/PetProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
