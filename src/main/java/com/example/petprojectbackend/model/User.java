b351b5c4d48df633aa078d596c06d50585e7a1ad
YezhovOleksandr <bladeagainst@gmail.com>
2023-02-08 00:50:29 +0200

    Creating User model

---
+++ src/main/java/com/example/petprojectbackend/model/User.java
@@ -0,0 +1,37 @@
+package com.example.petprojectbackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String first_name;
+
+    private String last_name;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    public User(String first_name, String last_name, String username, String email, String password) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
