cd6104b8bc2ef618b3823cfe17936cf80a12e15a
YezhovOleksandr <OleksandrYezhov19@gmail.com>
2023-02-12 15:22:33 +0200

    Creating frontend part and User model

---
+++ PetProject/src/main/java/com/example/petproject/models/User.java
@@ -0,0 +1,38 @@
+package com.example.petproject.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String first_name;
+
+    private String last_name;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    public User(String first_name, String last_name, String username, String password, String email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
