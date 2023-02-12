d8ab34cf34e69e4ee976605ae72bc887754c3bf8
YezhovOleksandr <OleksandrYezhov19@gmail.com>
2023-02-12 17:56:15 +0200

    Creating Register.css and Register page

--- PetProject/petproject-front/src/App.js
+++ PetProject/petproject-front/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Layout/Navbar";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Navbar/>
           <Routes>
+              <Route exact path={"/"} element={<Home/>}/>
               <Route exact path={"/register"} element={<Register/>}/>
               <Route exact path={"/login"} element={<Login/>}/>
           </Routes>
--- PetProject/petproject-front/src/pages/Register.js
+++ PetProject/petproject-front/src/pages/Register.js
@@ -1,7 +1,41 @@
+
+import '../css/Register.css'
+import React, {useState} from "react";
 export default function Register() {
+
+    const [user, setUser] = useState({
+        first_name:"",
+        last_name:"",
+        username:"",
+        password:"",
+        email:""
+    })
+
+    const {first_name, last_name, username, password, email} = user
+
+
     return (
-        <div>
-            <h2>Register form</h2>
+        <div className={"body-container"}>
+            <div className="login-page">
+                <div className="form">
+                    <form className="register-form">
+                        <input type="text" placeholder="First name" value={first_name}/>
+                        <input type="text" placeholder="Last name" value={last_name}/>
+                        <input type="text" placeholder="Username" value={username}/>
+                        <input type="password" placeholder="password" value={password}/>
+                        <input type="text" placeholder="email address" value={email}/>
+                        <button>create</button>
+                        <p className="message">Already registered? <a href="#">Sign In</a></p>
+                    </form>
+                    {/*<form className="login-form">*/}
+                    {/*    <input type="text" placeholder="username"/>*/}
+                    {/*    <input type="password" placeholder="password"/>*/}
+                    {/*    <button>login</button>*/}
+                    {/*    <p className="message">Not registered? <a href="#">Create an account</a></p>*/}
+                    {/*</form>*/}
+                </div>
+            </div>
+
         </div>
     );
 }
