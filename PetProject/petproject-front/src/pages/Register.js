044992c313aa37a855f0c949e524f390c459c116
YezhovOleksandr <OleksandrYezhov19@gmail.com>
2023-02-13 18:55:16 +0200

    Adding axios to front and completing Register.js page

--- PetProject/petproject-front/src/pages/Register.js
+++ PetProject/petproject-front/src/pages/Register.js
@@ -1,36 +1,44 @@
 
 import '../css/Register.css'
 import React, {useState} from "react";
+import axios from "axios";
+import {useNavigate, Link} from "react-router-dom";
 export default function Register() {
 
-    const [user,setUser] = useState({
+    let navigate = useNavigate();
+
+    const  [user,setUser]=useState({
         first_name:"",
         last_name:"",
         username:"",
         password:"",
         email:""
     })
-    const {first_name, last_name, username, password, email} = user
+    const{first_name, last_name, username, password, email} = user
 
-    const onInputChange=(e)=>{
-        e.preventDefault();
-        setUser({...user,[e.target.name]:e.target.value})
+    const onInputChange=(e  )=>{
+        setUser({...user, [e.target.name]:e.target.value})
     }
 
+    const onSubmit= async (e)=>{
+        e.preventDefault()
+        await axios.post("http://localhost:8080/register", user)
+        navigate("/login")
+    }
 
 
     return (
         <div className={"body-container"}>
             <div className="login-page">
                 <div className="form">
-                    <form className="register-form">
-                        <input type="text" placeholder="First name" value={first_name} onChange={(e)=>onInputChange(e)}/>
-                        <input type="text" placeholder="Last name" value={last_name} onChange={(e)=>onInputChange(e)}/>
-                        <input type="text" placeholder="Username" value={username} onChange={(e)=>onInputChange(e)}/>
-                        <input type="password" placeholder="password" value={password} onChange={(e)=>onInputChange(e)}/>
-                        <input type="text" placeholder="email address" value={email} onChange={(e)=>onInputChange(e)}/>
-                        <button>create</button>
-                        <p className="message">Already registered? <a href="#">Sign In</a></p>
+                    <form className="register-form" onSubmit={(e)=>onSubmit(e)}>
+                        <input type={"text"} placeholder="First name" name={"first_name"} value={first_name} onChange={(e)=>onInputChange(e)}/>
+                        <input type={"text"} placeholder="Last name" name={"last_name"} value={last_name} onChange={(e)=>onInputChange(e)}/>
+                        <input type={"text"} placeholder="Username"  name={"username"} value={username} onChange={(e)=>onInputChange(e)}/>
+                        <input type={"password"} placeholder="password"  name={"password"} value={password} onChange={(e)=>onInputChange(e)}/>
+                        <input type={"text"} placeholder="email address" name={"email"} value={email} onChange={(e)=>onInputChange(e)}/>
+                        <button type={"submit"}>create</button>
+                        <p className="message">Already registered? <Link to={"/login"}>Sign In</Link></p>
                     </form>
                     {/*<form className="login-form">*/}
                     {/*    <input type="text" placeholder="username"/>*/}
--- PetProject/src/main/java/com/example/petproject/controllers/UserController.java
+++ PetProject/src/main/java/com/example/petproject/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:3000/")
 @AllArgsConstructor
 public class UserController {
 
