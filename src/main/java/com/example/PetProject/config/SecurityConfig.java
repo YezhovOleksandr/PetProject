175d1bcec70eb3f96ddfef75ddf159b611936c85
YezhovOleksandr <OleksandrYezhov19@gmail.com>
2023-03-17 21:55:12 +0200

    Adding JwtAuthenticationFilter, create CurrentUser interface to decrease dependence on Spring Security, updating SecurityConfig

--- src/main/java/com/example/PetProject/config/SecurityConfig.java
+++ src/main/java/com/example/PetProject/config/SecurityConfig.java
@@ -1,8 +1,16 @@
 package com.example.PetProject.config;
 
+import com.example.PetProject.security.CustomUserDetailsService;
+import com.example.PetProject.security.JwtAuthenticationEntryPoint;
+import com.example.PetProject.security.JwtAuthenticationFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -11,7 +19,29 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+@AllArgsConstructor
 public class SecurityConfig  {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    private final JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
