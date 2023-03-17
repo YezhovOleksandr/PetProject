d81d5de748093464672e7578a904557b6c6050e9
YezhovOleksandr <OleksandrYezhov19@gmail.com>
2023-03-17 22:55:37 +0200

    updating SecurityConfig

--- src/main/java/com/example/PetProject/config/SecurityConfig.java
+++ src/main/java/com/example/PetProject/config/SecurityConfig.java
@@ -4,18 +4,25 @@ import com.example.PetProject.security.CustomUserDetailsService;
 import com.example.PetProject.security.JwtAuthenticationEntryPoint;
 import com.example.PetProject.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -36,22 +43,60 @@ public class SecurityConfig  {
         return new JwtAuthenticationFilter();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
             throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Autowired
+    public void registerProvider(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+
+    }
+
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeRequests(auth -> {
-                    auth.antMatchers("/home").permitAll();
-                })
-                .httpBasic(Customizer.withDefaults())
-                .build();
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests((auth) ->{
+                    auth.antMatchers("/",
+                            "favicon.ico",
+                            "/**/*.png",
+                            "/**/*.gif",
+                            "/**/*.svg",
+                            "/**/*.jpg",
+                            "/**/*.html",
+                            "/**/*.css",
+                            "/**/*.js")
+                            .permitAll();
+                    auth.antMatchers("/api/auth/**")
+                            .permitAll();
+                    auth.antMatchers("/api/user/checkUsernameAvailability",
+                            "/api/user/checkEmailAvailability")
+                            .permitAll();
+                    auth.antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
+                            .permitAll();
+                    auth.anyRequest().authenticated();
+                }).build();
     }
 
 }
