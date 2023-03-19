b5a7dd6e9ca5012a3df137033b2529525fc02569
YezhovOleksandr <OleksandrYezhov19@gmail.com>
2023-03-19 17:05:54 +0200

    Creating ChoiceVoteCount and VoteRepository

---
+++ src/main/java/com/example/PetProject/models/ChoiceVoteCount.java
@@ -0,0 +1,13 @@
+package com.example.PetProject.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ChoiceVoteCount {
+    private Long choiceId;
+    private Long voteCount;
+
+
+}
