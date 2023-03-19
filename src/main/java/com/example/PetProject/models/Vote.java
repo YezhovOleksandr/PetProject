28669c780ecc9d8054ee03e0e403bebf90b9a0e8
YezhovOleksandr <OleksandrYezhov19@gmail.com>
2023-03-19 14:55:01 +0200

    Creating Choice, Poll and Vote models

---
+++ src/main/java/com/example/PetProject/models/Vote.java
@@ -0,0 +1,31 @@
+package com.example.PetProject.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "votes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "poll_id",
+                "user_id"
+        })
+})
+@Data
+public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "poll_id", nullable = false)
+    private Poll poll;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "poll_id", nullable = false)
+    private Choice choice;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
