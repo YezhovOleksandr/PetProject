1e219f6350844a424623a18c8bda8be8ed32adde
YezhovOleksandr <OleksandrYezhov19@gmail.com>
2023-03-19 15:44:50 +0200

    Creating PollRepository

---
+++ src/main/java/com/example/PetProject/repository/PollRepository.java
@@ -0,0 +1,23 @@
+package com.example.PetProject.repository;
+
+import com.example.PetProject.models.Poll;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import java.util.Optional;
+
+@Repository
+public interface PollRepository extends JpaRepository<Poll, Long> {
+    Optional<Poll> findById(Long pollId);
+
+    Page<Poll> findByCreatedBy(Long userId, Pageable pageable);
+
+    long countByCreatedBy(Long userId);
+
+    List<Poll> findByIdIn(List<Long> pollIds);
+
+    List<Poll> findByIdIn(List<Long> pollIds, Sort sort);
+}
